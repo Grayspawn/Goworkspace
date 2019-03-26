59bad564d3fb8c30ad7a928a69ce007b7df4675e
Grayspawn <lneco117@gmail.com>
2019-03-26 11:45:37 -0500

    Update 04 & Identifier

--- ToddMc/05-blank-identifier/01-invalid-code/main.go
+++ ToddMc/05-blank-identifier/01-invalid-code/main.go
@@ -1,2 +1,13 @@
-package _1_invalid_code
+package main
+
+import "fmt"
+
+func main () {
+	a:= "stored in a"
+	b:= "stored in b"  // declared b
+
+	fmt.Println("a - ", a)
+	// fmt.Println("b - ", b) - with b variable commented out the program will not run because b is not being used. See red underline
+}
+
 
--- ToddMc/05-blank-identifier/02-http.get-example/01-with-error-checking/main.go
+++ ToddMc/05-blank-identifier/02-http.get-example/01-with-error-checking/main.go
@@ -1,1 +1,24 @@
-package _1_with_error_checking
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main () {
+
+	res, err := http.Get("http://www.kwanji.com")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	fmt.Printf("%s", page)
+}
