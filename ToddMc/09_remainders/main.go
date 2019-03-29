94e4d0e56669e53b7a949319be63753244a06d41
Grayspawn <lneco117@gmail.com>
2019-03-29 09:57:20 -0500

    Remainders

--- ToddMc/09_remainders/main.go
+++ ToddMc/09_remainders/main.go
@@ -1,1 +1,26 @@
-package _9_remainders
+package main
+
+import "fmt"
+
+func main () {
+	x := 13 / 3 // result is 4
+	fmt.Println(x)
+
+	y := 13 % 3
+	fmt.Println(y) // result is 1 using the remainder operator '&'
+
+	if y == 1 {
+		fmt.Println("Odd")
+	} else {
+		fmt.Println("Even")
+	}
+
+
+	for i :=1; i <23; i++ {
+		if i%2 == 1 {
+			fmt.Println("Still Odd")
+		} else {
+			fmt.Println("Now very even")
+		}
+	}
+}
