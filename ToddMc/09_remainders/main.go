14f4833daa84528fde5455f1b92874b9222d9bd2
Grayspawn <lneco117@gmail.com>
2019-04-29 15:46:39 -0500

    9&10

--- ToddMc/09_remainders/main.go
+++ ToddMc/09_remainders/main.go
@@ -1,1 +1,23 @@
-package _9_remainders
+package main
+
+import "fmt"
+
+func main () {
+
+	x := 13 % 3 // % = remainder command
+	fmt.Println(x)
+
+	if x == 1{
+		fmt.Println("Odd")
+	} else {
+		fmt.Print("Even")
+	}
+
+	for i := 0; i < 100; i++ {
+		if i % 2 == 1 {
+			fmt.Println("Odd")
+		} else {
+			fmt.Println("Even")
+		}
+	}
+}
